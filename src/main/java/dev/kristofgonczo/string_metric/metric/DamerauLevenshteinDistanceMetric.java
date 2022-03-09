34d9389b703ba3eea51e5e5eca0c13cad2a31372
Kristóf Göncző <kristofgonczo@gmail.com>
2022-03-09 13:25:15 +0100

    DamerauLevenshteinDistanceMetric implementation and testing

--- src/main/java/dev/kristofgonczo/string_metric/metric/DamerauLevenshteinDistanceMetric.java
+++ src/main/java/dev/kristofgonczo/string_metric/metric/DamerauLevenshteinDistanceMetric.java
@@ -6,6 +6,27 @@ class DamerauLevenshteinDistanceMetric implements StringMetric {
 
     @Override
     public double measure(@NonNull String string1, @NonNull String string2) {
-        return 0;
+        int[][] d = new int[string1.length() + 1][string2.length() + 1];
+
+        for (int i = 0; i < string1.length() + 1; i++) {
+            d[i][0] = i;
+        }
+        for (int j = 0; j < string2.length() + 1; j++) {
+            d[0][j] = j;
+        }
+
+        for (int i = 1; i < string1.length() + 1; i++) {
+            for (int j = 1; j < string2.length() + 1; j++) {
+                int cost = string1.charAt(i - 1) == string2.charAt(j - 1) ? 0 : 1;
+                d[i][j] = Math.min(Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + cost);
+                if (i > 1 &&
+                        j > 1 &&
+                        string1.charAt(i - 1) == string2.charAt(j - 2) &&
+                        string1.charAt(i - 2) == string2.charAt(j - 1)) {
+                    d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
+                }
+            }
+        }
+        return d[string1.length()][string2.length()];
     }
 }
--- src/test/java/dev/kristofgonczo/string_metric/metric/DamerauLevenshteinDistanceMetricTest.java
+++ src/test/java/dev/kristofgonczo/string_metric/metric/DamerauLevenshteinDistanceMetricTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DamerauLevenshteinDistanceMetricTest {
 
@@ -17,5 +18,13 @@ public class DamerauLevenshteinDistanceMetricTest {
     @Test
     void measureTest() {
         assertDoesNotThrow(() -> damerauLevenshteinDistanceMetric.measure("", ""));
+
+        assertEquals(0, damerauLevenshteinDistanceMetric.measure("abc","abc"));
+        assertEquals(1, damerauLevenshteinDistanceMetric.measure("ac", "ca"));
+        assertEquals(2, damerauLevenshteinDistanceMetric.measure("abc", "cba"));
+        assertEquals(2, damerauLevenshteinDistanceMetric.measure("10011","00101"));
+        assertEquals(2, damerauLevenshteinDistanceMetric.measure("bccbbcb", "ccbbccb"));
+        assertEquals(5, damerauLevenshteinDistanceMetric.measure("abcdef", "cba"));
+        assertEquals(6, damerauLevenshteinDistanceMetric.measure("abcdef", "ghijkl"));
     }
 }
